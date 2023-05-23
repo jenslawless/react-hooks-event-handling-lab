14ad49d18e077d60875e93fccc67a35ff08043ef
Jensen Lawless <jensenlawless@gmail.com>
2023-05-23 12:06:00 -0600

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+
+    function blurOut() {
+        console.log("Hey! Eyes on me!")
+    }
+    function focusOut() {
+        console.log("Good!")
+    }
+
+
+    return (
+        <button onFocus={focusOut} onBlur={blurOut}  >Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
